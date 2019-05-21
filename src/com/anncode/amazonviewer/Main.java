ae38bd70536aef2a168aba8d060f5d2363db7824
Alberto Avantes <alberto@got-itsolutions.com>
2019-05-21 18:04:56 -0500

    Primer commit

---
+++ src/com/anncode/amazonviewer/Main.java
@@ -0,0 +1,20 @@
+package com.anncode.amazonviewer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int exit = 0;
+		do {
+			System.out.println("BIENVENIDOS AMAZON VIEWER");
+			System.err.println("");
+			System.out.println("Selecciona el numero de la opción deseada");
+			System.out.println("1. Movies");
+			System.out.println("2. Series");
+			System.out.println("3. Books");
+			System.out.println("4. Magazines");
+			System.out.println("0. Exit");
+		}while(exit != 0);
+
+	}
+
+}
