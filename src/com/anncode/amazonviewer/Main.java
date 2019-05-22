3246ae69cb409a580d26cc7e85dab07b53230c0d
Alberto Avantes <alberto@got-itsolutions.com>
2019-05-22 18:49:30 -0500

    Nuevo POO

--- src/com/anncode/amazonviewer/Main.java
+++ src/com/anncode/amazonviewer/Main.java
@@ -1,8 +1,20 @@
 package com.anncode.amazonviewer;
 
+import java.util.Date;
+import java.util.Scanner;
+
+import com.anncode.amazonviewer.model.Movie;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Movie movie = new Movie("Coco", "Animation",(short) 2017);
+		movie.showData();
+//		showMenu(scan);
+	}
+	
+	public static void showMenu(Scanner scan) {
 		int exit = 0;
 		do {
 			System.out.println("BIENVENIDOS AMAZON VIEWER");
@@ -12,9 +24,95 @@ public class Main {
 			System.out.println("2. Series");
 			System.out.println("3. Books");
 			System.out.println("4. Magazines");
+			System.out.println("5. Report");
+			System.out.println("6. Report Today");
 			System.out.println("0. Exit");
+			
+			//Leer la respuesta del usuario
+			int response = scan.nextInt();
+			switch (response) {
+				case 0:
+					//salir
+					
+					break;
+				case 1:
+					showMovies();
+					break;
+				case 2:
+					showSeries();
+					break;
+				case 3:
+					showBooks();
+					break;
+				case 4:
+					showMagazines();
+					break;
+				case 5:
+					makeReport();
+					break;
+				case 6:
+					makeReport(new Date());
+					break;
+				default:
+					System.out.println();
+					System.out.println("....¡¡Selecciona una opción!!....");
+					System.out.println();
+					break;
+			}
+			
 		}while(exit != 0);
-
 	}
 
+	public static void showMovies() {
+		int exit = 0;
+		do {
+			System.out.println();
+			System.out.println(":: MOVIES ::");
+			System.out.println();
+		}while(exit != 0);
+	}
+	
+	public static void showSeries() {
+		int exit = 0;
+		do {
+			System.out.println();
+			System.out.println(":: SERIES ::");
+			System.out.println();
+		}while(exit != 0);
+	}
+	
+	public static void showBooks() {
+		int exit = 0;
+		do {
+			System.out.println();
+			System.out.println(":: BOOKS ::");
+			System.out.println();
+		}while(exit != 0);
+	}
+	
+	public static void showMagazines() {
+		int exit = 0;
+		do {
+			System.out.println();
+			System.out.println(":: MAGAZINES ::");
+			System.out.println();
+		}while(exit != 0);
+	}
+	
+	public static void showChapters() {
+		int exit = 0;
+		do {
+			System.out.println();
+			System.out.println(":: CHAPTERS ::");
+			System.out.println();
+		}while(exit != 0);
+	}
+	
+	public static void makeReport() {
+		
+	}
+	
+	public static void makeReport(Date date) {
+		
+	}
 }
